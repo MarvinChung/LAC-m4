4993ae02aa3c63190fec6d727ac55b65ba6b0cf0
MarvinChung <marvin852316497@gmail.com>
2020-08-17 14:25:05 +0800

    make clean

--- LAC/LAC_512/code_gen/NTT.py
+++ LAC/LAC_512/code_gen/NTT.py
@@ -29,6 +29,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+lin = """
+/*
+BSD 2-Clause License
+
+Copyright (c) 2020, Cheng-Jhih Shi, Chi-Ming Chung, Vincent Hwang
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+"""
+
 macro = """.macro montgomery_mul a, b, lower, upper, tmp, M_inv, M
     smull.w \\lower, \\upper, \\a, \\b
     mul.w \\tmp, \\lower, \\M_inv
@@ -789,6 +819,6 @@ vpop.w {s16, s17}
 pop.w {r4-r12, pc}
 """
 
-NTT = macro + start + combine_1_2_3 + combine_4_5_6 + combine_7_8_9 + text + \
+NTT = lin + macro + start + combine_1_2_3 + combine_4_5_6 + combine_7_8_9 + text + \
                    combine_9_8_7 + combine_6_5_4 + combine_3_2_1 + End
 print(NTT)
