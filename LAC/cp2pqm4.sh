ac22457441222ca57f29bd3536bce6da2ce33b66
MarvinChung <marvin852316497@gmail.com>
2020-10-23 13:03:47 +0800

    add v3b

--- LAC/cp2pqm4.sh
+++ LAC/cp2pqm4.sh
@@ -1,4 +1,9 @@
-cp -r our-lac-256-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-256-NTT1024
-cp -r our-lac-192-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-192-NTT1024
-cp -r our-lac-128-NTT512/  ../pqm4/mupq/crypto_kem/our-lac-128-NTT512
-cp -r our-lac-LIGHT-NTT512/ ../pqm4/mupq/crypto_kem/our-lac-LIGHT-NTT512
+cp -r our-lac-256-v3a-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-256-v3a-NTT1024
+cp -r our-lac-192-v3a-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-192-v3a-NTT1024
+cp -r our-lac-128-v3a-NTT512/  ../pqm4/mupq/crypto_kem/our-lac-128-v3a-NTT512
+cp -r our-lac-LIGHT-v3a-NTT512/ ../pqm4/mupq/crypto_kem/our-lac-LIGHT-v3a-NTT512
+
+cp -r our-lac-256-v3b-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-256-v3b-NTT1024
+cp -r our-lac-192-v3b-NTT1024/ ../pqm4/mupq/crypto_kem/our-lac-192-v3b-NTT1024
+cp -r our-lac-128-v3b-NTT512/  ../pqm4/mupq/crypto_kem/our-lac-128-v3b-NTT512
+cp -r our-lac-LIGHT-v3b-NTT512/ ../pqm4/mupq/crypto_kem/our-lac-LIGHT-v3b-NTT512
